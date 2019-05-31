c83508400c7b6729551c0891ef81d243efbeb4c7
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-31 08:49:22 -0500

    Cambios en routing
    - Creación y configuración de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { ListPostsComponent } from './components/admin/list-posts/list-posts.component'
+
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'user/register', component: RegisterComponent },
+	{ path: 'user/login', component: LoginComponent },
+	{ path: 'user/profile', component: ProfileComponent },
+	{ path: 'admin/list-posts', component: ListPostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
