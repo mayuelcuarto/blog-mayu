dfcbbefbc0fdd43c4de57b762df0f578e6caf267
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-31 08:51:42 -0500

    Cambios en app_module
    - Importando Componentes Login, Register, Profile, Home, ListPosts y ModalPost.
    - Importando FormsModule y environment.
    - Importando  librerias de Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,46 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListPostsComponent } from './components/admin/list-posts/list-posts.component';
+import { ModalPostComponent } from './modals/modal-post/modal-post.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    ListPostsComponent,
+    ModalPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFirestore
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
