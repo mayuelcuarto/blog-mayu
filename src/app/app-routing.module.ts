ca2e8f543686e1193a06a7cb06b769f9c35495a4
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-08 02:06:20 -0500

    Cambios en routing
    - Añadiendo rutas para componente videojuegos y post.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { RegisterComponent } from './components/users/register/register.componen
 import { LoginComponent } from './components/users/login/login.component';
 import { ProfileComponent } from './components/users/profile/profile.component';
 import { ListPostsComponent } from './components/admin/list-posts/list-posts.component'
+import { PostComponent } from './components/post/post.component';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'user/register', component: RegisterComponent },
 	{ path: 'user/login', component: LoginComponent },
 	{ path: 'user/profile', component: ProfileComponent },
-	{ path: 'admin/list-posts', component: ListPostsComponent }
+	{ path: 'admin/list-posts', component: ListPostsComponent },
+	{ path: 'post/:titulo', component: PostComponent},
+	{ path: 'videojuegos', component: VideojuegosComponent }
 ];
 
 @NgModule({
