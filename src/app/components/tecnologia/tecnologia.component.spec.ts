663a0ddf400cf901c5f874086dd00355cc21816a
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-08 02:09:50 -0500

    Creación de componente tecnologia

---
+++ src/app/components/tecnologia/tecnologia.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TecnologiaComponent } from './tecnologia.component';
+
+describe('TecnologiaComponent', () => {
+  let component: TecnologiaComponent;
+  let fixture: ComponentFixture<TecnologiaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TecnologiaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TecnologiaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
