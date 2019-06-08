0abd16e493c60069ac2a240ac46ac63c2ff8a86f
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-08 02:07:51 -0500

    Cambios en app_module
    - Import de RouterModule.
    - Import de componentes: Post, Videojuegos y Tecnologia.
    - En la directiva de imports se añadieron algunas reglas para poder utilizar anclajes con el routerLink.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,9 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { PostComponent } from './components/post/post.component';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
+import { TecnologiaComponent } from './components/tecnologia/tecnologia.component';
 
 @NgModule({
   declarations: [
@@ -30,10 +34,37 @@ import { AngularFirestore } from '@angular/fire/firestore';
     ProfileComponent,
     HomeComponent,
     ListPostsComponent,
-    ModalPostComponent
+    ModalPostComponent,
+    PostComponent,
+    VideojuegosComponent,
+    TecnologiaComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+            [],
+            {
+                // Tell the router to use the hash instead of HTML5 pushstate.
+                useHash: true,
+ 
+                // In order to get anchor / fragment scrolling to work at all, we need to
+                // enable it on the router.
+                anchorScrolling: "enabled",
+ 
+                // Once the above is enabled, the fragment link will only work on the
+                // first click. This is because, by default, the Router ignores requests
+                // to navigate to the SAME URL that is currently rendered. Unfortunately,
+                // the fragment scrolling is powered by Navigation Events. As such, we
+                // have to tell the Router to re-trigger the Navigation Events even if we
+                // are navigating to the same URL.
+                onSameUrlNavigation: "reload",
+ 
+                // Let's enable tracing so that we can see the aforementioned Navigation
+                // Events when the fragment is clicked.
+                enableTracing: true,
+                scrollPositionRestoration: "enabled"
+            }
+        ),
     AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
