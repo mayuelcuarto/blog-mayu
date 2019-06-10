f6239fec5cc21211ee4759906ea01ba8c0be43d9
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-10 02:04:44 -0500

    Agregando ruta de PostsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/users/login/login.component';
 import { ProfileComponent } from './components/users/profile/profile.component';
 import { ListPostsComponent } from './components/admin/list-posts/list-posts.component'
 import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
 import { TecnologiaComponent } from './components/tecnologia/tecnologia.component';
 import { Page404Component } from './components/page404/page404.component';
@@ -18,6 +19,7 @@ const routes: Routes = [
 	{ path: 'user/profile', component: ProfileComponent },
 	{ path: 'admin/list-posts', component: ListPostsComponent },
 	{ path: 'post/:id', component: PostComponent},
+	{ path: 'posts/:year/:month', component: PostsComponent },
 	{ path: 'videojuegos', component: VideojuegosComponent },
 	{ path: 'tecnologia', component: TecnologiaComponent },
 	{ path: '**', component: Page404Component }
