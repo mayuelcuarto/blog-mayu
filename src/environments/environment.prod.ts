4cda037fbd8ac1c6f65c6409dc60bb943f5ad7c8
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-11 23:00:14 -0500

    Seteando datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBFbaW21v8WiZiwRecCxWyr0RMWITgSI98",
+    authDomain: "blogmayu-a1ffd.firebaseapp.com",
+    databaseURL: "https://blogmayu-a1ffd.firebaseio.com",
+    projectId: "blogmayu-a1ffd",
+    storageBucket: "blogmayu-a1ffd.appspot.com",
+    messagingSenderId: "932547514225"
+  }
 };
