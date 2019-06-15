335d00ecf2032ca23e2d6a5d282c6c628712aaaf
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-06-15 06:33:18 -0500

    Optimización de código

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -24,63 +24,6 @@ export class PostsComponent implements OnInit {
   getPostsByYearMonth(year: number, month: number): void{
   	this.postService.getPostsByYearMonth(year, month).subscribe(posts => {
   		this.posts = posts;
-  		console.log("posts", posts);
   	});
-  }
-
-  mesString(month: number){
-    switch (month) {
-      case 1:
-        return "Enero";
-        break;
-
-      case 2:
-        return "Febrero";
-        break;
-
-      case 3:
-        return "Marzo";
-        break;
-
-      case 4:
-        return "Abril";
-        break;
-
-      case 5:
-        return "Mayo";
-        break;
-
-      case 6:
-        return "Junio";
-        break;
-
-      case 7:
-        return "Julio";
-        break;
-
-      case 8:
-        return "Agosto";
-        break;
-
-      case 9:
-        return "Setiembre";
-        break;
-
-      case 10:
-        return "Octubre";
-        break;
-
-      case 11:
-        return "Noviembre";
-        break;
-
-      case 12:
-        return "Diciembre";
-        break;
-      
-      default:
-        // code...
-        break;
-    }
   }
 }
