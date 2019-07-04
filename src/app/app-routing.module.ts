5a79046280668b25b427f790c472e695de8d911a
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-07-04 00:19:40 -0500

    Cambios en app-routing
    - Añadiendo rutas para LoginMessage y AuthGuard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/users/register/register.component';
 import { LoginComponent } from './components/users/login/login.component';
+import { LoginMessageComponent } from './components/users/login-message/login-message.component';
 import { ProfileComponent } from './components/users/profile/profile.component';
 import { ListPostsComponent } from './components/admin/list-posts/list-posts.component'
 import { PostComponent } from './components/post/post.component';
@@ -14,12 +15,15 @@ import { Page404Component } from './components/page404/page404.component';
 import { ArchivePostsComponent } from './components/archive-posts/archive-posts.component';
 import { SearchTituloComponent } from './components/search-titulo/search-titulo.component';
 
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'user/register', component: RegisterComponent },
 	{ path: 'user/login', component: LoginComponent },
-	{ path: 'user/profile', component: ProfileComponent },
-	{ path: 'admin/list-posts', component: ListPostsComponent },
+	{ path: 'user/login-message', component: LoginMessageComponent },
+	{ path: 'user/profile', component: ProfileComponent, canActivate: [AuthGuard] },
+	{ path: 'admin/list-posts', component: ListPostsComponent, canActivate: [AuthGuard] },
 	{ path: 'post/:id', component: PostComponent},
 	{ path: 'posts/:year/:month', component: PostsComponent },
 	{ path: 'videojuegos', component: VideojuegosComponent },
