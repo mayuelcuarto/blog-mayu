ba325eb80553a05fade49be2b206015f65d968f3
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-07-11 21:57:08 -0500

    Cambios en posts_ts
    - Optimización en código para forzar el cambio de datos al detectar modificaciones en los parámetros del url.

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -11,13 +11,24 @@ export class PostsComponent implements OnInit {
 
   constructor(
     private postService: PostService, 
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute
+    ) { }
   public posts = [];
 
   ngOnInit() {
-  	const year = this.route.snapshot.params['year'];
-  	const month = this.route.snapshot.params['month'];
-  	this.getPostsByCategoriaYearMonth('videojuegos', year, month);
+    this.route.params
+    .forEach(params => {
+    // This will be triggered every time the params change
+    // Add your code to reload here. i.e.
+      const year = params['year'];
+      const month = params['month'];
+
+      this.getPostsByCategoriaYearMonth('videojuegos', year, month);
+    });
+    /*
+  	let year = this.route.snapshot.params['year'];
+  	let month = this.route.snapshot.params['month'];
+  	this.getPostsByCategoriaYearMonth('videojuegos', year, month);*/
   }
 
   getPostsByCategoriaYearMonth(categoria: string, year: number, month: number): void{
